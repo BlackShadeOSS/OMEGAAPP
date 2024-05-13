7dab6e63397aee6218ef44e31080942450a0d16b
BlackShade <diaxsio10@gmail.com>
2024-05-13 12:32:57 +0200

    Update database.sql with table structure changes, data insertions, and index modifications for various tables. Include new data entries for likes and posts tables. Add constraints and auto-increment adjustments.

--- database.sql
+++ database.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 05 Maj 2024, 12:53
--- Wersja serwera: 10.4.25-MariaDB
--- Wersja PHP: 8.1.10
+-- Generation Time: May 13, 2024 at 12:32 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `omegaapp`
+-- Database: `omegaapp`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `community_note`
+-- Table structure for table `community_note`
 --
 
 CREATE TABLE `community_note` (
@@ -34,16 +34,17 @@ CREATE TABLE `community_note` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Zrzut danych tabeli `community_note`
+-- Dumping data for table `community_note`
 --
 
 INSERT INTO `community_note` (`id`, `cn_post_id`, `content`) VALUES
-(1, 1, 'dsadadsadasd');
+(1, 1, 'dsadadsadasd'),
+(2, 13, 'Kłamstwa');
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `fa_manager`
+-- Table structure for table `fa_manager`
 --
 
 CREATE TABLE `fa_manager` (
@@ -55,7 +56,7 @@ CREATE TABLE `fa_manager` (
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `firm_account`
+-- Table structure for table `firm_account`
 --
 
 CREATE TABLE `firm_account` (
@@ -69,7 +70,7 @@ CREATE TABLE `firm_account` (
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `follow`
+-- Table structure for table `follow`
 --
 
 CREATE TABLE `follow` (
@@ -80,23 +81,39 @@ CREATE TABLE `follow` (
   `firm_follower_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `follow`
+--
+
+INSERT INTO `follow` (`follow_id`, `user_ac_id`, `user_follower_id`, `firm_ac_id`, `firm_follower_id`) VALUES
+(5, 2, 2, NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `like`
+-- Table structure for table `likes`
 --
 
-CREATE TABLE `like` (
-  `follow_id` int(11) NOT NULL,
+CREATE TABLE `likes` (
+  `like_id` int(11) NOT NULL,
   `post_id` int(11) NOT NULL,
-  `user_follower_id` int(11) DEFAULT NULL,
-  `firm_follower_id` int(11) DEFAULT NULL
+  `user_id` int(11) DEFAULT NULL,
+  `firm_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `likes`
+--
+
+INSERT INTO `likes` (`like_id`, `post_id`, `user_id`, `firm_id`) VALUES
+(1, 7, NULL, NULL),
+(2, 7, NULL, NULL),
+(25, 12, 4, NULL);
+
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `posts`
+-- Table structure for table `posts`
 --
 
 CREATE TABLE `posts` (
@@ -113,22 +130,26 @@ CREATE TABLE `posts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Zrzut danych tabeli `posts`
+-- Dumping data for table `posts`
 --
 
 INSERT INTO `posts` (`post_id`, `author`, `author_firm`, `content`, `likes`, `shares`, `comments_number`, `file_id`, `post_id_for_comment`, `create_datetime`) VALUES
-(4, 1, NULL, 'jhdfgjdfgjfd', 0, 0, 0, '66339a04c608c', NULL, '2024-05-05 12:53:17'),
 (5, 1, NULL, 'dfsadsadsadsadas', 0, 0, 0, '6633a35b8458d1.00942415', NULL, '2024-05-05 12:53:17'),
 (6, 2, NULL, 'dsadasdas', 0, 0, 0, '6633b48dbb40c8.29140694', NULL, '2024-05-05 12:53:17'),
-(7, 2, NULL, 'dasdwfasrdffgwaef', 0, 0, 0, '6635399cedaa49.40825932', NULL, '2024-05-05 12:53:17'),
+(7, 2, NULL, 'dasdwfasrdffgwaef', 3, 0, 0, '6635399cedaa49.40825932', NULL, '2024-05-05 12:53:17'),
 (8, 2, NULL, 'dsadasdsad', 0, 0, 0, '663618ce560ca3.32491856', NULL, '2024-05-05 12:53:17'),
 (9, 2, NULL, 'ewqeqweqweqeqeqwedsada', 0, 0, 0, '663619836e9b82.19645908', 8, '2024-05-05 12:53:17'),
-(10, 2, NULL, 'dsadsadasdfdas', 0, 0, 0, '663762d24f6361.17536704', 9, '2024-05-05 12:53:17');
+(10, 2, NULL, 'dsadsadasdfdas', 0, 0, 0, '663762d24f6361.17536704', 9, '2024-05-05 12:53:17'),
+(11, 2, NULL, 'rfewwfewfw', 0, 0, 0, '6639170e6efd94.19247592', 7, '2024-05-06 19:44:46'),
+(12, 4, NULL, 'dasdasdasdasd', 1, 0, 0, '663a42b3ea2706.58391838', NULL, '2024-05-07 17:03:16'),
+(13, 4, NULL, 'sdaDASdsfagdsf', 0, 0, 0, '663a50db60f284.66740036', 12, '2024-05-07 18:03:39'),
+(14, 4, NULL, 'Kozak', 0, 0, 0, NULL, 13, '2024-05-08 14:57:37'),
+(15, 4, NULL, 'WUT\r\n', 0, 0, 0, NULL, 12, '2024-05-08 18:28:48');
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `user_account`
+-- Table structure for table `user_account`
 --
 
 CREATE TABLE `user_account` (
@@ -139,24 +160,26 @@ CREATE TABLE `user_account` (
   `followers` int(11) NOT NULL,
   `is_cn_admin` tinyint(1) NOT NULL DEFAULT 0,
   `is_verified` tinyint(1) NOT NULL DEFAULT 0,
-  `avatar_id` varchar(23) DEFAULT NULL
+  `avatar_id` varchar(23) DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Zrzut danych tabeli `user_account`
+-- Dumping data for table `user_account`
 --
 
-INSERT INTO `user_account` (`user_ac_id`, `username`, `password`, `email`, `followers`, `is_cn_admin`, `is_verified`, `avatar_id`) VALUES
-(1, '123', '$2y$10$f/LiaveNrDzCyOHJKPgZd.JZQjQ6fWUBrFZduvAWw94.rK8zAZExW', '123@gmail.com', 0, 1, 0, NULL),
-(2, 'morda1123', '$2y$10$E/ypNgrKxWG5hZK6YOnyIe6WwfZfPxqCEby8ImC5.LiLMG2JeweG6', 'diaxsio10@gmail.com', 0, 0, 0, '6633a89eaf25e4.86468926'),
-(3, 'BlackShade', '$2y$10$HE0HUi1EgPpm7sTUUtBUbuCqn9tD5DVUXrxSKUjymoauI/30uTQAa', 'diaxsio10@gmail.com', 0, 0, 0, '66353963410ff1.06804830');
+INSERT INTO `user_account` (`user_ac_id`, `username`, `password`, `email`, `followers`, `is_cn_admin`, `is_verified`, `avatar_id`, `created`) VALUES
+(1, '123', '$2y$10$f/LiaveNrDzCyOHJKPgZd.JZQjQ6fWUBrFZduvAWw94.rK8zAZExW', '123@gmail.com', 0, 1, 0, NULL, '2024-05-10 11:28:50'),
+(2, 'morda1123', '$2y$10$E/ypNgrKxWG5hZK6YOnyIe6WwfZfPxqCEby8ImC5.LiLMG2JeweG6', 'diaxsio10@gmail.com', 0, 0, 0, '6633a89eaf25e4.86468926', '2024-05-10 11:28:50'),
+(3, 'BlackShade', '$2y$10$HE0HUi1EgPpm7sTUUtBUbuCqn9tD5DVUXrxSKUjymoauI/30uTQAa', 'diaxsio10@gmail.com', 0, 0, 0, '66353963410ff1.06804830', '2024-05-10 11:28:50'),
+(4, 'BlackShadeOSS', '$2y$10$vM8Rxk5y2V0VCbfdSqIcRem3aTFFbzxl8lu8SqaBpJFzW.yJdP9Vy', 'diaxsio10@gmail.com', 0, 0, 0, '663a1bed80e7a0.30707126', '2024-05-10 11:28:50');
 
 --
--- Indeksy dla zrzutów tabel
+-- Indexes for dumped tables
 --
 
 --
--- Indeksy dla tabeli `community_note`
+-- Indexes for table `community_note`
 --
 ALTER TABLE `community_note`
   ADD PRIMARY KEY (`id`),
@@ -164,7 +187,7 @@ ALTER TABLE `community_note`
   ADD KEY `community_note_fk1` (`cn_post_id`);
 
 --
--- Indeksy dla tabeli `fa_manager`
+-- Indexes for table `fa_manager`
 --
 ALTER TABLE `fa_manager`
   ADD PRIMARY KEY (`manager_id`),
@@ -173,14 +196,14 @@ ALTER TABLE `fa_manager`
   ADD KEY `fa_manager_fk2` (`user_id`);
 
 --
--- Indeksy dla tabeli `firm_account`
+-- Indexes for table `firm_account`
 --
 ALTER TABLE `firm_account`
   ADD PRIMARY KEY (`firm_ac_id`),
   ADD UNIQUE KEY `firm_ac_id` (`firm_ac_id`);
 
 --
--- Indeksy dla tabeli `follow`
+-- Indexes for table `follow`
 --
 ALTER TABLE `follow`
   ADD PRIMARY KEY (`follow_id`),
@@ -191,17 +214,17 @@ ALTER TABLE `follow`
   ADD KEY `follow_fk4` (`firm_follower_id`);
 
 --
--- Indeksy dla tabeli `like`
+-- Indexes for table `likes`
 --
-ALTER TABLE `like`
-  ADD PRIMARY KEY (`follow_id`),
-  ADD UNIQUE KEY `follow_id` (`follow_id`),
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`like_id`),
+  ADD UNIQUE KEY `follow_id` (`like_id`),
   ADD KEY `like_fk1` (`post_id`),
-  ADD KEY `like_fk2` (`user_follower_id`),
-  ADD KEY `like_fk3` (`firm_follower_id`);
+  ADD KEY `like_fk2` (`user_id`),
+  ADD KEY `like_fk3` (`firm_id`);
 
 --
--- Indeksy dla tabeli `posts`
+-- Indexes for table `posts`
 --
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`post_id`),
@@ -211,77 +234,77 @@ ALTER TABLE `posts`
   ADD KEY `posts_fk8` (`post_id_for_comment`);
 
 --
--- Indeksy dla tabeli `user_account`
+-- Indexes for table `user_account`
 --
 ALTER TABLE `user_account`
   ADD PRIMARY KEY (`user_ac_id`),
   ADD UNIQUE KEY `user_ac_id` (`user_ac_id`);
 
 --
--- AUTO_INCREMENT dla zrzuconych tabel
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT dla tabeli `community_note`
+-- AUTO_INCREMENT for table `community_note`
 --
 ALTER TABLE `community_note`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT dla tabeli `fa_manager`
+-- AUTO_INCREMENT for table `fa_manager`
 --
 ALTER TABLE `fa_manager`
   MODIFY `manager_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT dla tabeli `firm_account`
+-- AUTO_INCREMENT for table `firm_account`
 --
 ALTER TABLE `firm_account`
   MODIFY `firm_ac_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT dla tabeli `follow`
+-- AUTO_INCREMENT for table `follow`
 --
 ALTER TABLE `follow`
-  MODIFY `follow_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `follow_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
--- AUTO_INCREMENT dla tabeli `like`
+-- AUTO_INCREMENT for table `likes`
 --
-ALTER TABLE `like`
-  MODIFY `follow_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `likes`
+  MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
--- AUTO_INCREMENT dla tabeli `posts`
+-- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT dla tabeli `user_account`
+-- AUTO_INCREMENT for table `user_account`
 --
 ALTER TABLE `user_account`
-  MODIFY `user_ac_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_ac_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- Ograniczenia dla zrzutów tabel
+-- Constraints for dumped tables
 --
 
 --
--- Ograniczenia dla tabeli `community_note`
+-- Constraints for table `community_note`
 --
 ALTER TABLE `community_note`
   ADD CONSTRAINT `community_note_fk1` FOREIGN KEY (`cn_post_id`) REFERENCES `posts` (`post_id`);
 
 --
--- Ograniczenia dla tabeli `fa_manager`
+-- Constraints for table `fa_manager`
 --
 ALTER TABLE `fa_manager`
   ADD CONSTRAINT `fa_manager_fk1` FOREIGN KEY (`firm_id`) REFERENCES `firm_account` (`firm_ac_id`),
   ADD CONSTRAINT `fa_manager_fk2` FOREIGN KEY (`user_id`) REFERENCES `user_account` (`user_ac_id`);
 
 --
--- Ograniczenia dla tabeli `follow`
+-- Constraints for table `follow`
 --
 ALTER TABLE `follow`
   ADD CONSTRAINT `follow_fk1` FOREIGN KEY (`user_ac_id`) REFERENCES `user_account` (`user_ac_id`),
@@ -290,15 +313,15 @@ ALTER TABLE `follow`
   ADD CONSTRAINT `follow_fk4` FOREIGN KEY (`firm_follower_id`) REFERENCES `firm_account` (`firm_ac_id`);
 
 --
--- Ograniczenia dla tabeli `like`
+-- Constraints for table `likes`
 --
-ALTER TABLE `like`
+ALTER TABLE `likes`
   ADD CONSTRAINT `like_fk1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`),
-  ADD CONSTRAINT `like_fk2` FOREIGN KEY (`user_follower_id`) REFERENCES `user_account` (`user_ac_id`),
-  ADD CONSTRAINT `like_fk3` FOREIGN KEY (`firm_follower_id`) REFERENCES `firm_account` (`firm_ac_id`);
+  ADD CONSTRAINT `like_fk2` FOREIGN KEY (`user_id`) REFERENCES `user_account` (`user_ac_id`),
+  ADD CONSTRAINT `like_fk3` FOREIGN KEY (`firm_id`) REFERENCES `firm_account` (`firm_ac_id`);
 
 --
--- Ograniczenia dla tabeli `posts`
+-- Constraints for table `posts`
 --
 ALTER TABLE `posts`
   ADD CONSTRAINT `posts_fk1` FOREIGN KEY (`author`) REFERENCES `user_account` (`user_ac_id`),
